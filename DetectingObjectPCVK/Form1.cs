dac1dcfc54b09c8480d820bf7face3394691458d
dcopetz <krisaditya11@gmail.com>
2020-04-30 21:40:38 +0700

    menambahkan fungsi kamera dan load form

--- DetectingObjectPCVK/Form1.cs
+++ DetectingObjectPCVK/Form1.cs
@@ -21,9 +21,126 @@ namespace DetectingObjectPCVK
 {
     public partial class Form1 : Form
     {
+        FilterInfoCollection _device;
+        VideoCaptureDevice _CaptureDevice;
+        Bitmap _bitmapEdgeImage, _bitmapBinaryImage, _bitmapGreyImage, _bitmapBlurImage, _colorFilterImage;
+        //ColorFiltering _colorFilter = new ColorFiltering();
+        EuclideanColorFiltering _colorFilter = new EuclideanColorFiltering();
+        System.Drawing.Font _font = new System.Drawing.Font("Times New Roman", 48, FontStyle.Bold);
+        System.Drawing.SolidBrush _brush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+        SobelEdgeDetector _edgeFilter = new SobelEdgeDetector();
+        Pen _PictureboxPen = new Pen(Color.Black, 5);
+
+        bool _blurFlag = false;
+        int ipenWidth = 5, iFeatureWidth;
+        int iThreshold = 40, iRadius = 40;
+        int iColorMode = 1, iRedValue = 220, iGreenValue = 30, iBlueValue = 30;
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        #region Form_Load, Closing
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _device = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                for (var i = 0; i < _device.Count; i++)
+                    comboBox1.Items.Add(_device[i].Name);              
+                                   
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                StopCameras();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+             
+        #region MyMethods
+        private void SrartCameras(int deviceindex)
+        {
+            try
+            {
+                _CaptureDevice = new VideoCaptureDevice(_device[deviceindex].MonikerString);
+                _CaptureDevice.NewFrame += new NewFrameEventHandler(get_Frame);
+                //Start the Capture Device
+                _CaptureDevice.Start();
+
+                listBox1.Items.Add("Webcam connect");
+                ScrollDown();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StopCameras()
+        {
+            try
+            {
+                _CaptureDevice.Stop();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
+        void ScrollDown()
+        {
+            try
+            {
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                listBox1.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void get_Frame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Insert image into Picuture Box
+            Bitmap _BsourceFrame = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = BlobDetection(_BsourceFrame);
+            pictureBox2.Image = _bitmapEdgeImage;
+            pictureBox3.Image = _bitmapBinaryImage;
+            pictureBox4.Image = _colorFilterImage;        
+        }
+
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+
+        private void sbRedColor_Scroll(object sender, ScrollEventArgs e)
+        {
+            iRedValue = sbRedColor.Value;
+
+            listBox1.Items.Add("Red: " + iRedValue.ToString());
+            ScrollDown();
         }
 
         #region Form_Load, Closing
