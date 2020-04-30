5b47694204654fa7be187383434a926ebfed1cf0
HernandaCandraP <hernandacp@gmail.com>
2020-04-30 14:20:31 +0700

    Menambahkan Tampilan Form1 dan Deklarasi Fungsi

--- DetectingObjectPCVK/Form1.Designer.cs
+++ DetectingObjectPCVK/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace DetectingObjectPCVK
+﻿using System;
+using System.Windows.Forms;
+
+namespace DetectingObjectPCVK
 {
     partial class Form1
     {
@@ -28,13 +31,292 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.sbThreshold = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.sbGreenColor = new System.Windows.Forms.HScrollBar();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.sbRedColor = new System.Windows.Forms.HScrollBar();
+            this.sbBlueColor = new System.Windows.Forms.HScrollBar();
+            this.sbRadius = new System.Windows.Forms.HScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 60);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(282, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(306, 60);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(16, 98);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(282, 344);
+            this.listBox1.TabIndex = 12;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(714, 60);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(306, 470);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(714, 470);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // sbThreshold
+            // 
+            this.sbThreshold.Location = new System.Drawing.Point(91, 28);
+            this.sbThreshold.Maximum = 255;
+            this.sbThreshold.Name = "sbThreshold";
+            this.sbThreshold.Size = new System.Drawing.Size(188, 22);
+            this.sbThreshold.TabIndex = 17;
+            this.sbThreshold.Value = 40;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Threshold";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbGreen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rbBlue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.sbGreenColor);
+            this.groupBox1.Controls.Add(this.rbRed);
+            this.groupBox1.Controls.Add(this.sbRedColor);
+            this.groupBox1.Controls.Add(this.sbBlueColor);
+            this.groupBox1.Controls.Add(this.sbRadius);
+            this.groupBox1.Controls.Add(this.sbThreshold);
+            this.groupBox1.Location = new System.Drawing.Point(16, 472);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(282, 315);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Option";
+            // 
+            // rbGreen
+            // 
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(8, 252);
+            this.rbGreen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(79, 24);
+            this.rbGreen.TabIndex = 20;
+            this.rbGreen.TabStop = true;
+            this.rbGreen.Text = "Green";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Raudius";
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(8, 192);
+            this.rbBlue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(66, 24);
+            this.rbBlue.TabIndex = 20;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Blue";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // sbGreenColor
+            // 
+            this.sbGreenColor.Location = new System.Drawing.Point(91, 242);
+            this.sbGreenColor.Maximum = 255;
+            this.sbGreenColor.Name = "sbGreenColor";
+            this.sbGreenColor.Size = new System.Drawing.Size(188, 22);
+            this.sbGreenColor.TabIndex = 17;
+            this.sbGreenColor.Value = 30;
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Checked = true;
+            this.rbRed.Location = new System.Drawing.Point(8, 135);
+            this.rbRed.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(64, 24);
+            this.rbRed.TabIndex = 19;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Red";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // sbRedColor
+            // 
+            this.sbRedColor.Location = new System.Drawing.Point(91, 135);
+            this.sbRedColor.Maximum = 255;
+            this.sbRedColor.Name = "sbRedColor";
+            this.sbRedColor.Size = new System.Drawing.Size(188, 22);
+            this.sbRedColor.TabIndex = 17;
+            this.sbRedColor.Value = 220;
+            // 
+            // sbBlueColor
+            // 
+            this.sbBlueColor.Location = new System.Drawing.Point(91, 192);
+            this.sbBlueColor.Maximum = 255;
+            this.sbBlueColor.Name = "sbBlueColor";
+            this.sbBlueColor.Size = new System.Drawing.Size(188, 22);
+            this.sbBlueColor.TabIndex = 17;
+            this.sbBlueColor.Value = 30;
+            // 
+            // sbRadius
+            // 
+            this.sbRadius.Location = new System.Drawing.Point(91, 80);
+            this.sbRadius.Maximum = 450;
+            this.sbRadius.Name = "sbRadius";
+            this.sbRadius.Size = new System.Drawing.Size(188, 22);
+            this.sbRadius.TabIndex = 17;
+            this.sbRadius.Value = 100;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(306, 420);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 35);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(266, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(700, 39);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Detecting Object PCVK";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1140, 899);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Detecting Object PCVK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.HScrollBar sbThreshold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.HScrollBar sbRadius;
+        private System.Windows.Forms.HScrollBar sbGreenColor;
+        private System.Windows.Forms.HScrollBar sbBlueColor;
+        private System.Windows.Forms.HScrollBar sbRedColor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- DetectingObjectPCVK/Form1.cs
+++ DetectingObjectPCVK/Form1.cs
@@ -7,6 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
+using System.Drawing.Imaging;
+using AForge;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
 
 namespace DetectingObjectPCVK
 {
@@ -16,5 +25,126 @@ namespace DetectingObjectPCVK
         {
             InitializeComponent();
         }
+
+        #region Form_Load, Closing
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region MyMethods
+        private void SrartCameras(int deviceindex)
+        {
+
+        }
+
+        private void StopCameras()
+        {
+
+        }
+
+        void ScrollDown()
+        {
+
+        }
+
+        private void get_Frame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+        }
+
+        //private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        //{
+
+        //}
+
+        private void sbRedColor_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void sbBlueColor_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void sbGreenColor_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbRed_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbBlue_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbGreen_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sbRadius_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sbThreshold_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private double FindDistance(int _pixel)
+        {
+            ///
+            /// distance(D): distance of object from the camera
+            /// _focalLength(F): focal length of camera
+            /// _pixel(P): apparent width in pixel
+            /// _ObjectWidth(W): width of object
+            /// 
+            /// F = (P*D)/W
+            ///     -> D = (W*F)/P
+            return _pixel;
+        }
+        #endregion
+
+        #region Blob Detection
+        /// <summary> Blob Detection    
+        /// This method for color object detection by Blob counter algorithm.
+        /// If you using this method, then you can detecting as follows:
+        ///             red circle, rectangle, triangle
+        ///             blue circle, rectangle, triangle
+        ///             green circle, rectangle, triangle
+        /// the process of this method as follow:
+        ///     1. color filtering by Euclidean filtering(R, G, B).
+        ///     2. the grayscale filtering based on color filtered image.
+        ///     3. the binary filtering based on edge filter image.
+        ///     4. Finally, detecting object, distance from the camera and degree are expreed on picturebox 1.
+        /// </summary>
+
+        //private Bitmap BlobDetection(Bitmap _bitmapSourceImage)
+        //{
+            
+        //}
+        #endregion
     }
 }
